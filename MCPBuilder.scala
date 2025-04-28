f0cea3a32db059d8b2eefe11fbb2a5db209c3976
Anton Sviridov <keynmol@gmail.com>
2025-04-28 17:16:25 +0100

    WIP

---
+++ MCPBuilder.scala
@@ -0,0 +1,72 @@
+package mcp
+
+import upickle.default.*
+import java.io.{BufferedReader, InputStreamReader}
+import upickle.core.TraceVisitor.TraceException
+
+class MCPBuilder private (
+    requestHandlers: Map[String, ujson.Value => ujson.Value | Error]
+):
+  def handleRequest(req: MCPRequest)(f: req.In => req.Out | Error) =
+    val handler = (in: ujson.Value) =>
+      val params = read[req.In](in)
+      f(params) match
+        case e: Error => writeJs(e)
+        case e        => writeJs(e.asInstanceOf[req.Out])
+    new MCPBuilder(requestHandlers.updated(req.method, handler))
+  end handleRequest
+
+  def process(is: java.io.InputStream) =
+
+    val reader = new BufferedReader(new InputStreamReader(is))
+
+    def out[J: Writer](j: J) =
+      val obj = writeJs(j)
+      obj("jsonrpc") = "2.0"
+      System.err.println(s"Outputting ${obj}")
+      System.out.println(write(obj))
+    end out
+
+    var line: String = null
+    while { line = reader.readLine(); line != null } do
+      try
+        val json = read[ujson.Obj](line)
+        System.err.println(s"Receiving $json")
+        if json.value.contains("id") && json.value.contains("method")
+        then // it's a request
+          val method = json.value("method").str
+          val id = json.value("id")
+
+          requestHandlers.get(method) match
+            case None =>
+              out(
+                Error(
+                  ErrorCode.MethodNotFound,
+                  s"Method $method is not handled"
+                )
+              )
+
+            case Some(value) =>
+              value(json) match
+                case err: Error =>
+                  out(err)
+
+                case response: ujson.Value =>
+                  out(
+                    Response(id, Some(writeJs(response)))
+                  )
+          end match
+        end if
+      catch
+        case e: TraceException =>
+          out(Error(ErrorCode.InvalidRequest, e.getMessage))
+        case e =>
+          out(Error(ErrorCode.InternalError, e.getMessage))
+
+    end while
+  end process
+
+end MCPBuilder
+
+object MCPBuilder:
+  def create(): MCPBuilder = new MCPBuilder(Map.empty)
---
+++ jsonrpc.scala
@@ -0,0 +1,51 @@
+package mcp
+
+import mcp.{*, given}
+
+import java.io.{BufferedReader, InputStreamReader}
+import upickle.default.*
+
+case class Request(
+    id: ujson.Value,
+    method: String,
+    params: Option[ujson.Value] = None
+) derives upickle.default.Reader
+
+enum ErrorCode(val value: Int):
+  // Standard JSON-RPC error codes
+  case ParseError extends ErrorCode(-32700)
+  case InvalidRequest extends ErrorCode(-32600)
+  case MethodNotFound extends ErrorCode(-32601)
+  case InvalidParams extends ErrorCode(-32602)
+  case InternalError extends ErrorCode(-32603)
+end ErrorCode
+
+object ErrorCode:
+  private val mapping = ErrorCode.values.map(ec => ec.value -> ec).toMap
+
+  given ReadWriter[ErrorCode] =
+    summon[ReadWriter[Int]].bimap(
+      _.value,
+      code =>
+        mapping.getOrElse(
+          code,
+          throw new IllegalArgumentException(s"Unknown error code $code")
+        )
+    )
+end ErrorCode
+
+case class Error(
+    code: ErrorCode,
+    message: String,
+    data: Option[ujson.Value] = None,
+) derives upickle.default.ReadWriter
+
+case class Response(
+    id: ujson.Value,
+    result: Option[ujson.Value] = None,
+) derives ReadWriter
+
+case class Notification(
+    method: String,
+    params: Option[ujson.Value] = None,
+) derives ReadWriter
